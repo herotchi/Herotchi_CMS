9fda2c5f91c26796db223bbcb6040082de58b611
herotchi <hidenochion+github@gmail.com>
2023-11-06 15:01:51 +0900

    管理側大カテゴリ一覧画面実装

--- app/Models/FirstCategory.php
+++ app/Models/FirstCategory.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Consts\FirstCategoryConsts;
+use Illuminate\Support\Arr;
+
 class FirstCategory extends Model
 {
     use HasFactory;
@@ -22,5 +25,19 @@ class FirstCategory extends Model
         $this->fill($data);
 
         $this->save();
+    }
+
+
+    public function getAdminList(array $data)
+    {
+        $query = $this::query();
+
+        $query->when(Arr::exists($data, 'name') && $data['name'], function ($query) use ($data) {
+            return $query->where('name', 'like', "%{$data['name']}%");
+        });
+
+        $lists = $query->paginate(FirstCategoryConsts::PAGENATE_LIST_LIMIT);
+
+        return $lists;
     }
 }
