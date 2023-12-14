d644ca13ab6d6fc4bdbdb91e2613379b85669df0
herotchi <hidenochion+github@gmail.com>
2023-12-14 14:20:57 +0900

    公開側トップ画面カルーセル実装

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Media;
+
 class TopController extends Controller
 {
     //
     public function top()
     {
-        return view('top');
+        $mediaModel = new Media();
+        $carousels = $mediaModel->getCarousels();
+
+        return view('top', compact('carousels'));
     }
 }
